27d36144e450d6f7e9200172b6ac2fe26f59db8b
juliabush <j.elizabethbush@gmail.com>
2025-05-17 17:32:36 +0200

    Creating 16 divs and file for css

---
+++ main.js
@@ -0,0 +1,28 @@
+// const div1 = document.createElement("div");
+// const div2 = document.createElement("div");
+// const div3 = document.createElement("div");
+// const div4 = document.createElement("div");
+// const div5 = document.createElement("div");
+// const div6 = document.createElement("div");
+// const div7 = document.createElement("div");
+// const div8 = document.createElement("div");
+// const div9 = document.createElement("div");
+// const div10 = document.createElement("div");
+// const div11 = document.createElement("div");
+// const div12 = document.createElement("div");
+// const div13 = document.createElement("div");
+// const div14 = document.createElement("div");
+// const div15 = document.createElement("div");
+// const div16 = document.createElement("div");
+
+// const containerDiv = document.getElementById("container-div");
+
+// Here is a more concise way of doing what i just did above
+
+const containerDiv = document.getElementById("container-div");
+
+for (let i = 0; i < 16; i++) {
+  const div = document.createElement("div");
+  div.textContent = `Div ${i + 1}`;
+  containerDiv.appendChild(div);
+}
